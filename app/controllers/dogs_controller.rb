1b884b063c1e66fffa62bf7962adc80f861603f0
Mabelys Arias <MabelysA@gmail.com>
2021-06-28 15:19:19 -0400

    adds controller and route

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,16 @@
+class DogsController < ApplicationController
+  def create
+    dog = Dogs.new(
+      name: params[:name],
+      age: params[:email],
+      breed: params[:breed],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if dog.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: dog.errors.full_messages }, status: :bad_request
+    end
+  end
+end
