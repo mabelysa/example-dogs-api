7a1fdaea3a17130db98ade499d892dee7011c1d2
Mabelys Arias <MabelysA@gmail.com>
2021-06-28 23:51:33 -0400

    fixes dog model

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,17 +1,41 @@
 class DogsController < ApplicationController
-  def create
-    dog = Dog.new(
-      name: params[:name],
-      age: params[:email],
-      breed: params[:breed],
-    )
+  def index
     if current_user
-      if dog.save
-        render json: { message: "User created successfully" }, status: :created
-      else
-        render json: []
-        # render json: { errors: dog.errors.full_messages }, status: :bad_request
-      end
+      dogs = Dog.all
+      render json: dogs.as_json
+    else
+      render json: []
     end
   end
+
+  def create
+    if current_user
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed],
+      )
+      dog.save
+      render json: dog.as_json
+    else
+      render json: []
+    end
+  end
+
+  # def create
+  #   dog = Dog.new(
+  #     name: params[:name],
+  #     age: params[:age],
+  #     breed: params[:breed],
+  #   )
+  #   if current_user
+  #     if dog.save
+  #       render json: dog
+  #     else
+  #       render json: { errors: dog.errors.full_messages }
+  #     end
+  #   else
+  #     render json: {}
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   post "/dogs" => "dogs#create"
+  get "/dogs" => "dogs#index"
 end
